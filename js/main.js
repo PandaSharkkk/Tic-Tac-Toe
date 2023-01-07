3a841b99f94081940fe55cb736333a60fe9b6615
Davyn Paringkoan <davyn.par@gmail.com>
2023-01-06 19:22:17 -0800

    update commit

--- js/main.js
+++ js/main.js
@@ -107,5 +107,81 @@ function updateBoard(){
 }
 
 function checkWinner(){
+    //check if x win
+    if(boardArray[0][0] === 'X' && boardArray[0][1] === 'X' && boardArray[0][2] === 'X'){
+        endGame('X');
+    } else if(boardArray[1][0] === 'X' && boardArray[1][1] === 'X' && boardArray[1][2] === 'X') {
+        endGame('X');
+    } else if(boardArray[2][0] === 'X' && boardArray[2][1] === 'X' && boardArray[2][2] === 'X') {
+        endGame('X');
+    } else if(boardArray[0][0] === 'X' && boardArray[1][0] === 'X' && boardArray[2][0] === 'X') {
+        endGame('X');
+    } else if(boardArray[0][1] === 'X' && boardArray[1][1] === 'X' && boardArray[2][1] === 'X') {
+        endGame('X');
+    } else if(boardArray[0][2] === 'X' && boardArray[1][2] === 'X' && boardArray[2][2] === 'X') {
+        endGame('X');
+    } else if(boardArray[0][0] === 'X' && boardArray[1][1] === 'X' && boardArray[2][2] === 'X') {
+        endGame('X');
+    } else if(boardArray[0][2] === 'X' && boardArray[1][1] === 'X' && boardArray[2][0] === 'X') {
+        endGame('X');
+    }
 
+    //check if o win
+    if(boardArray[0][0] === 'O' && boardArray[0][1] === 'O' && boardArray[0][2] === 'O'){
+        endGame('O');
+    } else if(boardArray[1][0] === 'O' && boardArray[1][1] === 'O' && boardArray[1][2] === 'O') {
+        endGame('O');
+    } else if(boardArray[2][0] === 'O' && boardArray[2][1] === 'O' && boardArray[2][2] === 'O') {
+        endGame('O');
+    } else if(boardArray[0][0] === 'O' && boardArray[1][0] === 'O' && boardArray[2][0] === 'O') {
+        endGame('O');
+    } else if(boardArray[0][1] === 'O' && boardArray[1][1] === 'O' && boardArray[2][1] === 'O') {
+        endGame('O');
+    } else if(boardArray[0][2] === 'O' && boardArray[1][2] === 'O' && boardArray[2][2] === 'O') {
+        endGame('O');
+    } else if(boardArray[0][0] === 'O' && boardArray[1][1] === 'O' && boardArray[2][2] === 'O') {
+        endGame('O');
+    } else if(boardArray[0][2] === 'O' && boardArray[1][1] === 'O' && boardArray[2][0] === 'O') {
+        endGame('O');
+    }
+
+    //if board is full with no winner, game is tied
+    if(fullBoard()){
+        endGame('tie');
+    }
+}
+
+function endGame(winner){
+    //if x win, update score & reset board
+    if(winner === 'X'){
+        xScore ++;
+        document.getElementById("xScore").innerHTML = "X: " + xScore;
+        restartFunction();
+    }
+
+    //if o win, update score & reset board
+    if(winner === 'O'){
+        oScore ++;
+        document.getElementById("oScore").innerHTML = "O: " + oScore;
+        restartFunction();
+    }
+
+    //if tie, update score & reset board
+    if(winner === 'tie'){
+        tieScore ++;
+        document.getElementById("tieScore").innerHTML = "Tie: " + tieScore;
+        restartFunction();
+    }
+}
+
+function fullBoard(){
+    let count = 0;
+    for(let x = 0; x < 3; x++){
+        for(let y = 0; y < 3; y++){
+            if(boardArray[x][y] === ''){
+                count++;
+            }
+        }
+    }
+    return count === 0;
 }
